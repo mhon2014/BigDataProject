de8f390431bbdd4eb047191ab7b748a087a8ddd6
Michael Gourdine <michaelrge78@gmail.com>
2022-12-02 12:26:41 -0500

    Added more filter options
    Can now filter for countries and sensor platforms

--- Application/client/src/FilterBox.js
+++ Application/client/src/FilterBox.js
@@ -6,42 +6,91 @@ import {useContext, useEffect} from "react";
 import { MultiSelect } from "react-multi-select-component";
 
 export default function FilterBox() {
-  const {setData, selectFilter, setSelectFilter, allPoints, categories} = useContext(Context);
+  const {
+    setData, selectCategory, setSelectCategory,
+    selectCountry, setSelectCountry, allPoints,
+    selectSensor, setSelectSensor, sensors,
+    categories, countries
+  } = useContext(Context);
 
   // Updates the map to show the filtered/unfiltered
   useEffect(() => {
     // No filter applied
-    if (selectFilter.length === 0) {
+    if (selectCategory.length === 0 && selectCountry.length === 0 && selectSensor.length === 0) {
       setData([]);
-    }
-    // All filters applied
-    else if (selectFilter.length === 62) {
-      setData(allPoints);
     }
     // Some filters applied
     else {
       // Extract the category names to match the json format
-      const currList = selectFilter.map(x => x.value);
+      const filterCategory = selectCategory.map(x => x.value);
+      const filterCountry = selectCountry.map(x => x.value);
+      const filterSensor = selectSensor.map(x => x.value);
 
-      // Filters for the given filter conditions
-      const result = allPoints.filter(function (point) {
-        return currList.includes(point.category);
+      let filtered = []
+
+      // Filters for the given filter category conditions
+      filtered = allPoints.filter(function (point) {
+        return filterCategory.includes(point.category);
       });
 
+      // Filters for the given filter country code conditions
+      if (selectCountry.length > 0 && filtered.length !== 0) {
+        filtered = filtered.filter(function (point) {
+          return filterCountry.includes(point.country_code);
+        });
+      }
+      else if (selectCountry.length > 0 && filtered.length === 0) {
+        filtered = allPoints.filter(function (point) {
+          return filterCountry.includes(point.country_code);
+        });
+      }
+
+      // Filters for the given filter sensor conditions
+      if (selectSensor.length > 0 && filtered.length !== 0) {
+        filtered = filtered.filter(function (point) {
+          return filterSensor.includes(point.sensor_platform_name);
+        });
+      }
+      else if (selectSensor.length > 0 && filtered.length === 0) {
+        filtered = allPoints.filter(function (point) {
+          return filterSensor.includes(point.sensor_platform_name);
+        });
+      }
+
       // Update the new points
-      setData(result);
+      setData(filtered);
     }
     // eslint-disable-next-line
-  }, [selectFilter]);
+  }, [selectCategory, selectCountry, selectSensor]);
 
   return (
     <div className="filter_box">
       <form className="form_extend">
         <MultiSelect
+          className="category"
           selectAll
           options={categories}
-          value={selectFilter}
-          onChange={setSelectFilter}
+          value={selectCategory}
+          onChange={setSelectCategory}
+          overrideStrings={{"selectSomeItems":'Select Category...'}}
+          labelledBy="Select"
+        />
+        <MultiSelect
+          className="country"
+          selectAll
+          options={countries}
+          value={selectCountry}
+          onChange={setSelectCountry}
+          overrideStrings={{"selectSomeItems":'Select Country...'}}
+          labelledBy="Select"
+        />
+        <MultiSelect
+          className="sensor"
+          selectAll
+          options={sensors}
+          value={selectSensor}
+          onChange={setSelectSensor}
+          overrideStrings={{"selectSomeItems":'Select Sensor...'}}
           labelledBy="Select"
         />
       </form>
