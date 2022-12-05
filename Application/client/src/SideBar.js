6feff402d93bd1f7cbae9c77d42e0f4ed53ad57a
Michael James Hon <mhon2014@my.fit.edu>
2022-12-05 02:01:40 -0500

    got lazy on css lol

--- Application/client/src/SideBar.js
+++ Application/client/src/SideBar.js
@@ -1,56 +1,61 @@
-import { Context } from './Context';
+import { Context } from "./Context";
 
-import './Sidebar.css';
+import "./Sidebar.css";
 
-import {useContext} from "react";
-import {useSpring, animated} from 'react-spring'
+import { useContext } from "react";
+import { useSpring, animated } from "react-spring";
 // ${ToggleState?'sidebarActive':null}`
 
 export default function SideBar() {
-  const {toggle, setToggle, locationInfo} = useContext(Context);
+  const { toggle, setToggle, locationInfo } = useContext(Context);
 
   const translate = useSpring({
-    transform: toggle ? `translateX(0%)` : `translateX(100%)`
+    transform: toggle ? `translateX(0%)` : `translateX(100%)`,
     // transition: 'background 0.1s'
   });
 
-    const close = () => {
-        setToggle(false)
-    }
-    
+  const close = () => {
+    setToggle(false);
+  };
 
-    // const renderrecursive = (nested) => {
-    //     return nested.map((item) => (
-    //         <div>
-    //             {item.title}
-    //             {item.child && renderrecursive(item.child)}
-    //         </div>
-    //     ))
-    //   }
-    
+  // const renderrecursive = (nested) => {
+  //     return nested.map((item) => (
+  //         <div>
+  //             {item.title}
+  //             {item.child && renderrecursive(item.child)}
+  //         </div>
+  //     ))
+  //   }
+
   return (
-    <animated.div className='sidebar' style={translate}>
-    <button name='close' onClick={close} >X</button>
-    <div>
-        <div id='imageDiv'>
-        {locationInfo != null && <img
-        src={locationInfo.image_url}
-                onError={(e) => (
-                    (e.target.onerror = null)
-                    (e.target.src = "")
-                )}
-                aria-label="image"
-                ></img>
-        }
-        {/* <ul>{LocationInfo.name}</ul> */}
+    <animated.div className="sidebar" style={translate}>
+      <button name="close" onClick={close}>
+        X
+      </button>
+      <div>
+        <div id="imageDiv">
+          {locationInfo != null && (
+            <img
+              src={locationInfo.image_url}
+              onError={(e) => (e.target.onerror = null)((e.target.src = ""))}
+              aria-label="image"
+            ></img>
+          )}
+          {/* <ul>{LocationInfo.name}</ul> */}
         </div>
-        <div id='dataInfo'>
-            {
-                Object.keys(locationInfo).map((key, index) => 
-                (<li key={key}>{key}:{locationInfo[key]}</li>))
-            }
+        <div id="dataInfo">
+          <table>
+            {Object.keys(locationInfo).map((key, index) => (
+              <tr>
+                <td key={key}>
+                  <b>{key}:</b>
+                </td>
+                <td>{locationInfo[key]}</td>
+              </tr>
+            ))}
+          </table>
         </div>
-    </div>
+      </div>
     </animated.div>
   );
 }
